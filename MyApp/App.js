f06a90cdf57f87e8fb2aa1c40898bcef47fabb0f
Adipah Osei <adipahosei@gmail.com>
2024-05-24 04:29:11 +0000

    final commit

--- MyApp/App.js
+++ MyApp/App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>My name is Osei Kwabena Adipah</Text>
+      <Text style={styles.text}>
+        My name is <Text style = {styles.bold}>Osei Kwabena Adipah.</Text>
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -19,5 +21,8 @@ const styles = StyleSheet.create({
   },
   text: {
     fontSize: '24',
+  },
+  bold: {
+    fontWeight: 'bold',
   }
 });
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # rn-assignment2-ID-11288153
+
+## SCREENSHOOT OF THE APP
+
+![screenshot](<MyApp/assets/mobile app image.jpg>)
+
+## DESCRIPTION
+
+- Created a an app named "MyApp" with the react native framework.
+- In this app some simple modifications where made based on the instructions given.
+- Some modifications where; changing the background color, modifing the text to contain your name as shown above, change the font size etc.
+
+## ID - 11288153
